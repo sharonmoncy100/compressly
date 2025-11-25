b302f7d6ede2a43f0d470683ef50625e0fec3e10
sharonmoncy100 <sharonmoncy100@gmail.com>
2025-11-25 14:30:10 +0530

    Add Vercel Analytics (Analytics root component)

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+/* Vercel Analytics */
+import { Analytics } from "@vercel/analytics/react";
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* Place Analytics once at the root so it tracks all page views */}
+    <Analytics />
   </React.StrictMode>
 );
